4fd89ae82eea45c0b06fc43a9821f42c5c545d05
ict26 <ict26@DESKTOP-AN1RJPB>
2022-12-09 21:13:23 +0900

    연습

--- Javaproj/src/ex1/test/OmokTest.java
+++ Javaproj/src/ex1/test/OmokTest.java
@@ -4,17 +4,32 @@ public class OmokTest {
 
 	public static void main(String[] args) {
 
-		for (int i = 0; i < 10; i++) {
+//		for (int i = 0; i < 10; i++) {
+//
+//			for (int j = 0; j < 10; j++) {
+//				if (i == 2 && j == 2) {
+//					System.out.print('○');
+//				} else
+//					System.out.print('┼');
+//			}
+//			System.out.println();
+//
+//		}
 
-			for (int j = 0; j < 10; j++) {
-				if (i == 2 && j == 2) {
-					System.out.print('○');
-				} else 
-				System.out.print('┼');
+		for (int i = 0; i < 5; i++) {
+			for (int j = 0; j <= i; j++) {
+				System.out.print("*");
 			}
 			System.out.println();
-
 		}
+
+		for (int i = 0; i < 4; i++) {
+			for (int j = 0; j < 4 - i; j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+
 	}
 
 }
