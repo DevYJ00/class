b77f49e0f46e61aa31834d154f2cd41175fa3b06
DevYJ00 <peullolakeullin@gmail.com>
2022-12-12 18:10:03 +0900

    오목 만들기 1

--- Javaproj/src/ex1/test/OmokTest.java
+++ Javaproj/src/ex1/test/OmokTest.java
@@ -4,32 +4,39 @@ public class OmokTest {
 
 	public static void main(String[] args) {
 
-//		for (int i = 0; i < 10; i++) {
-//
-//			for (int j = 0; j < 10; j++) {
-//				if (i == 2 && j == 2) {
-//					System.out.print('○');
-//				} else
-//					System.out.print('┼');
-//			}
-//			System.out.println();
-//
-//		}
+		System.out.printf("%c", '┌');
+		for (int i = 1; i <= 10; i++)
 
-		for (int i = 0; i < 5; i++) {
-			for (int j = 0; j <= i; j++) {
-				System.out.print("*");
+			System.out.printf("%c", '┬');
+
+		System.out.printf("%c", '┐');
+		System.out.println();
+
+		for (int y = 1; y <= 10; y++) {
+
+			System.out.printf("%c", '├');
+
+			for (int x = 1; x <= 10; x++) {
+
+				if (x == 3 && y == 3)
+					System.out.printf("%c", '○');
+				else
+					System.out.printf("%c", '┼');
+
 			}
-			System.out.println();
-		}
+			// 오른쪽 테두리
+			System.out.printf("%c", '┤');
 
-		for (int i = 0; i < 4; i++) {
-			for (int j = 0; j < 4 - i; j++) {
-				System.out.print("*");
-			}
-			System.out.println();
-		}
+			System.out.println(); // 끌낼 때
 
+		}
+		
+		System.out.printf("%c", '└');
+		for (int i = 1; i <= 10; i++)
+
+			System.out.printf("%c", '┴');
+
+		System.out.printf("%c", '┘');
 	}
 
 }
